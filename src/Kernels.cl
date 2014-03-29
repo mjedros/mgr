cb5a61fd69a0309015faea944c1fa932d16d6366
michal <mjedros@gmail.com>
2014-03-29 23:40:17 +0100

    Dilation and formatting

--- src/Kernels.cl
+++ src/Kernels.cl
@@ -1,17 +1,39 @@
-constant sampler_t sampler =
-    CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_REPEAT | CLK_FILTER_NEAREST;
+constant sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_REPEAT | CLK_FILTER_NEAREST;
 
-__kernel void sobel_rgb(__read_only image2d_t imageIn,
-                        __write_only image2d_t imageOut) {
-  float out_color = 0.0;
+__kernel void sobel_rgb(__read_only image2d_t imageIn, __write_only image2d_t imageOut) {
+   float out_color = 0.0;
 
-  int2 image_coord = (int2) {get_global_id(0), get_global_id(1)};
+   int2 image_coord = (int2) {get_global_id(0), get_global_id(1)};
 
-  float pixel = read_imagef(imageIn, sampler, image_coord).x;
-  if (pixel > 0.3f) {
-    out_color = 1;
-  } else {
-    out_color = 0;
-  }
-  write_imagef(imageOut, image_coord, out_color);
+   float pixel = read_imagef(imageIn, sampler, image_coord).x;
+   if (pixel > 0.3f) {
+      out_color = 0;
+   } else {
+      out_color = 1;
+   }
+   write_imagef(imageOut, image_coord, out_color);
+}
+__constant int squareSize = 40;
+__kernel void Dilate(__read_only image2d_t imageIn, __write_only image2d_t imageOut) {
+   int2 image_coord = (int2) {get_global_id(0), get_global_id(1)};
+   float3 ellipseParams = {0, 10, 10};
+   float sine = sin(ellipseParams.x);
+   float cosine = cos(ellipseParams.x);
+   float Ri = ellipseParams.y;
+   float Rj = ellipseParams.z;
+   for (int i = -squareSize; i < squareSize; i++) {
+      for (int j = -squareSize; j < squareSize; j++) {
+         float m = i * cosine - j * sine;
+         float n = i * sine + j * cosine;
+         if (m * m + (Ri * Ri / Rj / Rj) * n * n < Ri * Ri) {
+            int2 ijCoord = image_coord + (int2) {i, j};
+            float inPixel = read_imagef(imageIn, sampler, ijCoord).x;
+            if (inPixel > 0.5) {
+               write_imagef(imageOut, image_coord, 1);
+               return;
+            }
+         }
+      }
+   }
+   write_imagef(imageOut, image_coord, 0);
 }
--- src/OpenCLManager.cpp
+++ src/OpenCLManager.cpp
@@ -9,73 +9,69 @@ OpenCLManager::OpenCLManager() { Platform::get(&platforms); }
 
 OpenCLManager::~OpenCLManager() {}
 
-void OpenCLManager::Configure(std::string kernelFileName,
-                              unsigned int platformId, unsigned int deviceId) {
-  try {
-    ChooseDevice(platformId, deviceId);
-    CreateContext(platformId);
-    ReadPrograms(kernelFileName);
-    queue = CommandQueue(context, processingDevice);
-  }
-  catch (std::string e) {
-    cout << "Configure error: " << e << endl;
-  }
+void OpenCLManager::Configure(std::string kernelFileName, unsigned int platformId,
+                              unsigned int deviceId) {
+   try {
+      ChooseDevice(platformId, deviceId);
+      CreateContext(platformId);
+      ReadPrograms(kernelFileName);
+      queue = CommandQueue(context, processingDevice);
+   }
+   catch (std::string e) {
+      cout << "Configure error: " << e << endl;
+   }
 }
 
 void OpenCLManager::ReadPrograms(std::string kernelFileName) {
-  ifstream file(kernelFileName);
-  if (!file.is_open()) {
-    throw std::string("Kernel file not loaded");
-  }
+   ifstream file(kernelFileName);
+   if (!file.is_open()) {
+      throw std::string("Kernel file not loaded");
+   }
 
-  std::string prog(istreambuf_iterator<char>(file),
-                   (istreambuf_iterator<char>()));
-  Program::Sources source =
-      Program::Sources(1, make_pair(prog.c_str(), prog.length() + 1));
-  program = Program(context, source);
-  try {
-    std::vector<Device> devices; // = {processingDevice};
-    platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &devices);
-    program.build(devices, "");
-  }
-  catch (Error &e) {
-    std::string buildLog =
-        program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(processingDevice);
-    throw std::string("Build error:" + buildLog);
-  }
+   std::string prog(istreambuf_iterator<char>(file), (istreambuf_iterator<char>()));
+   Program::Sources source = Program::Sources(1, make_pair(prog.c_str(), prog.length() + 1));
+   program = Program(context, source);
+   try {
+      std::vector<Device> devices; // = {processingDevice};
+      platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &devices);
+      program.build(devices, "");
+   }
+   catch (Error &e) {
+      std::string buildLog = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(processingDevice);
+      throw std::string("Build error:" + buildLog);
+   }
 }
 
 void OpenCLManager::CreateContext(int platformId) {
 
-  cl_context_properties context_properties[3] = {
-      CL_CONTEXT_PLATFORM, (cl_context_properties)(platforms[platformId])(), 0};
-  cl_device_type deviceType;
-  processingDevice.getInfo((cl_device_info)CL_DEVICE_TYPE, &deviceType);
-  context = Context(deviceType, context_properties);
+   cl_context_properties context_properties[3] = {
+       CL_CONTEXT_PLATFORM, (cl_context_properties)(platforms[platformId])(), 0};
+   cl_device_type deviceType;
+   processingDevice.getInfo((cl_device_info)CL_DEVICE_TYPE, &deviceType);
+   context = Context(deviceType, context_properties);
 }
 
-void OpenCLManager::ChooseDevice(const unsigned int platformId,
-                                 const unsigned int DeviceId) {
+void OpenCLManager::ChooseDevice(const unsigned int platformId, const unsigned int DeviceId) {
 
-  std::vector<Device> devices;
-  platforms[platformId].getDevices(CL_DEVICE_TYPE_GPU, &devices);
-  processingDevice = devices[DeviceId];
+   std::vector<Device> devices;
+   platforms[platformId].getDevices(CL_DEVICE_TYPE_GPU, &devices);
+   processingDevice = devices[DeviceId];
 }
 
 std::vector<std::string> OpenCLManager::ListPlatforms() {
-  std::vector<std::string> devicesInfo;
-  for (auto &platform : platforms) {
-    std::string name, version;
-    platform.getInfo((cl_platform_info)CL_PLATFORM_NAME, &name);
-    platform.getInfo((cl_platform_info)CL_PLATFORM_VERSION, &version);
-    std::vector<Device> devices;
-    platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
-    cl_device_type deviceType;
-    for (unsigned int j = 0; j < devices.size(); j++) {
-      devices[j].getInfo((cl_device_info)CL_DEVICE_TYPE, &deviceType);
-      std::string deviceT = "";
-      devicesInfo.push_back(deviceT + "-" + name + "," + version);
-    }
-  }
-  return devicesInfo;
+   std::vector<std::string> devicesInfo;
+   for (auto &platform : platforms) {
+      std::string name, version;
+      platform.getInfo((cl_platform_info)CL_PLATFORM_NAME, &name);
+      platform.getInfo((cl_platform_info)CL_PLATFORM_VERSION, &version);
+      std::vector<Device> devices;
+      platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+      cl_device_type deviceType;
+      for (unsigned int j = 0; j < devices.size(); j++) {
+         devices[j].getInfo((cl_device_info)CL_DEVICE_TYPE, &deviceType);
+         std::string deviceT = "";
+         devicesInfo.push_back(deviceT + "-" + name + "," + version);
+      }
+   }
+   return devicesInfo;
 }
